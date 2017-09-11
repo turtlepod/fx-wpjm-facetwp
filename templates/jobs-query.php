ec68596ac0a9261d415cc1e9f51ebd6740b9eda2
David <david@shellcreeper.com>
2017-09-11 09:59:11 +0700

    test pagination.

--- templates/jobs-query.php
+++ templates/jobs-query.php
@@ -1,7 +1,11 @@
 <?php
 return array(
-	'post_type' => 'job_listing',
-	'orderby' => array( 'menu_order' => 'ASC', 'date' => 'DESC' ),
-	'order' => 'asc',
-	'post_status' => 'publish'
+	'post_type'      => 'job_listing',
+	'orderby'        => array(
+		'menu_order'   => 'ASC',
+		'date'         => 'DESC',
+	),
+	'order'          => 'asc',
+	'posts_per_page' => 3,
+	'post_status'    => 'publish'
 );
